13688a238c695958a609d0c6201800d3937b6cc7
Anna <anna.r.german1410@gmail.com>
2024-12-02 23:38:21 +0300

    refactor: correct other js-functions

--- script.js
+++ script.js
@@ -2,57 +2,62 @@ window.addEventListener("load", list);
 
 function list() {
 
-    // массив для пунктов списка (исходный)
+    // массив объектов для пунктов списка (исходный)
     let listArr = [
         {id: 0, text: 'develop to-do-list app'},
         {id: 1, text: 'study English'},
         {id: 2, text: 'make cookies'},
     ];
 
-    //элементы формы
+    // элементы формы
     const form = document.querySelector(".add-new-task-form");
     const inputAddElem = document.querySelector(".form-input");
     const ulList = document.querySelector(".task-list"); 
 
-    //показать изначальную статистику
-    showStat(listArr.length);
+    // * ---- Вывести пункты списка из массива listArr (для примера)
+    listArr.forEach( (elem) => addElemToDOM(elem.id, elem.text) );
 
-    // вывести пункты списка из массива listArr (для примера)
-    listArr.forEach(function(elem) {
-        addItemToDOM(elem.id, elem.text);
-    });
+    // * ---- Показать изначальную статистику
+    showStats(listArr.length);
 
-    //очистить текст подсказки, если поле ввода в фокусе
-    inputAddElem.onfocus = function(){
-        inputAddElem.placeholder = "";
+    // * ----- Текст подсказки
+    // очистить, если поле ввода в фокусе
+    inputAddElem.addEventListener("focus", function() {
+            inputAddElem.placeholder = "";
+        }
+    );
+    
+    // показать, если не в фокусе
+    inputAddElem.addEventListener("blur", function() {
+            inputAddElem.placeholder = "Add New Task";
+        }
+    );
+
+    // * ----- Добавить новые пункты при нажатии на кнопку Add
+    form.addEventListener("submit", function(event) {
+            event.preventDefault(); // отменить перезагрузку страницы
+
+            let itemId = String(Date.now()); // уникальное значение ID для элемента
+            let itemText = inputAddElem.value; // введенное в поле значение
+
+            //запуск функций добавления элемента в DOM и в массив
+            addElemToArray(itemId, itemText);
+            addElemToDOM(itemId, itemText);
+
+            inputAddElem.value = ''; // очистить поле ввода
+            //console.log(listArr);
+        }
+    );
+
+    // * Функция добавления элемента в массив listArr: 
+    // принимает id элемента и его содержимое (text), возвращает объект ключ:значение
+    function addElemToArray(id, text) {
+        listArr.push( {id, text} );
     }
-    // показать текст подсказки, если не в фокусе
-    inputAddElem.onblur = function(){
-        inputAddElem.placeholder = "Add New Task";
-    }
 
-    // при нажатии на кнопку Add -> запуск функций добавления элемента в DOM, и в массив
-    form.onsubmit = function(event) {
-        event.preventDefault(); // отменить перезагрузку страницы (установлена по умолчанию)
-        let itemId = String(Date.now()); // уникальное значение ID для элемента
-        let itemText = inputAddElem.value; // введенное в поле значение
-
-        //запуск функций добавления элемента в DOM, и в массив
-        addItemToArray(itemId, itemText);
-        addItemToDOM(itemId, itemText);
-
-        inputAddElem.value = ''; // очистить поле ввода
-    }
-
-    // функция добавления элемента в массив listArr: принимает id элемента и его содержимое (text)
-    function addItemToArray(id, text) {
-        listArr.push({id, text});
-    }
-
-    // функция добавления элемента в DOM: принимает id элемента и его содержимое (text):
+    // * Функция добавления элемента в DOM: принимает id элемента и его содержимое (text):
     // возвращает пункт списка (элемент li) для каждого элемента массива,
-    // также запускает функцию подсчета статистики
-    function addItemToDOM(id, text) {
+    function addElemToDOM(id, text) {
 
         // создать элемент li
         let li = document.createElement('li');
@@ -82,95 +87,99 @@ function list() {
         spanButton.setAttribute("title", "Delete Task");
         li.append(spanButton);
 
-        // получить количество пунктов для вывода статистики
-        countItemsForStats();
+        // получить количество пунктов для вывода статистики и запустить функцию
+        let itemsToDo = countItemsForStats();
+        showStats(itemsToDo);
 
-        // на строку (тоггл + текст) вешаем обработчик - запускается функция завершения задачи (вычеркивание) или отмены  вычеркивания
-        label.addEventListener("click", function () { completeElem(li, divToggle, spanText) });
+        // на строку (тоггл + текст) вешаем обработчик 
+        // запускается функция завершения задачи (вычеркивание) или отмены вычеркивания
+        label.addEventListener("click", function () { 
+            completeElem(li, divToggle, spanText) 
+        });
     
         // на кнопку удаления вешаем обработчик - запуск функции удаления задачи из DOM и массива
-        spanButton.addEventListener("click", function () { deleteElem(li.dataset.id, li) });  
+        spanButton.addEventListener("click", function () { 
+            deleteElem(li.dataset.id, li) 
+        });  
     }
 
-    // функция вычеркивания элемента при выполнении (или отмены операции вычеркивания):
+    // * Функция вычеркивания элемента при выполнении (или отмены операции вычеркивания):
     // принимает сам элемент (li), div (c тогглом) и текст заметки
     function completeElem(elem, toggle, text) {
 
-        toggle.classList.toggle("toggle-completed"); // добавить класс тогглу (если нет) или удалить (если есть)
-        text.classList.toggle("item-completed"); // добавить класс тексту с заметкой (если нет) или удалить (если есть)
+        // добавить класс (если нет) или удалить (если есть)
+        toggle.classList.toggle("toggle-completed"); 
+        text.classList.toggle("item-completed");
         elem.classList.toggle("completed");
 
         // запуск функции подсчета невычеркнутых элементов
-        countItemsForStats();
+        let itemsToDo = countItemsForStats();
+        showStats(itemsToDo);
     }
 
-    // функция удаления элемента из DOM и массива: принимает id элемента и сам элемент
+    // * Функция удаления элемента из DOM и массива: принимает id элемента и сам элемент
     function deleteElem(id, item) {
 
         //удалить из DOM
         item.remove();
         
         // удалить из массива: найти элемент в массиве через его id, а затем его индекс в массиве
-        let elem = listArr.find(elem => elem.id == id);
+        let elem = listArr.find( (elem) => elem.id === id );
         let index = listArr.indexOf(elem);
         listArr.splice(index, 1); // начиная с определенного индекса, удалить 1 элемент
 
-        countItemsForStats();
+        // запуск функции подсчета невычеркнутых элементов
+        let itemsToDo = countItemsForStats();
+        showStats(itemsToDo);
     }
 
-    // функция подсчета количества невычеркнутых (не выполненных) элементов
+    // * Функция подсчета количества невычеркнутых (невыполненных) элементов
     function countItemsForStats() {
         
         // общее количество элементов
-        let elems = document.getElementsByClassName("task-list-item");
+        let liItems = document.getElementsByClassName("task-list-item");
 
         // количество элементов с классом "completed"
         let count = 0;
-        for(let i = 0; i < elems.length; i++) {
-            if(elems[i].classList.contains("completed")) {
+        for (let i = 0; i < liItems.length; i++) {
+            if (liItems[i].classList.contains("completed")) {
                 count++;
             }
         }
 
         // оставшиеся элементы - это невыполненные пункты списка
-        let itemsToDo = elems.length - count;
-
-        removeOldStat();
-        showStat(itemsToDo);
-    }
-
-    //* Функция удаления устаревшей статистики
-    function removeOldStat() {
-        document.querySelector(".stat-number").textContent = "";  
+        return liItems.length - count;
     }
 
     // * Функция сбора и вывода статистики внизу списка: 
     // принимает длину массива элементов списка, добавляет текст в блоки
-    function showStat(amountOfItems) {
+    function showStats(amountOfItems) {
+        removeOldStats();
+
         // значение
         let statBlock = document.querySelector(".stat-number");
-        let statNumber = getStatNumber(amountOfItems); 
+        let statNumber = getStatNumber(amountOfItems); // вызов функции
         statBlock.prepend(statNumber);
 
         // окончание слова task
         let endingBlock = document.querySelector(".ending");
-        let wordEnding = getWordEnding(amountOfItems);
+        let wordEnding = getWordEnding(amountOfItems); // вызов функции
         endingBlock.textContent = wordEnding;
     }
     
     // возвращает число для статистики
     function getStatNumber(amountOfItems) { 
-        return (amountOfItems == 0) ? "no" : amountOfItems;
+        return (amountOfItems === 0) ? "no" : amountOfItems;
     }
 
     // возвращает окончание слова "task" (ед. или мн. число)
     function getWordEnding(amountOfItems) {
-        switch(amountOfItems) {
-            case 1:
-                return "";
-            default:   
-                return "s";
-        }
+        return (amountOfItems === 1) ? "" : "s";
+    }
+
+    // * Функция удаления устаревшей статистики
+    function removeOldStats() {
+        document.querySelector(".stat-number").textContent = "";  
     }
 
 }
